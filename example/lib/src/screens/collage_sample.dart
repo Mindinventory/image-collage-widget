225d03a3528c84a9c29c08c836511f8d2eab155c
Sahil Hamirani <sahil.hamirani@mindinventory.com>
2022-07-15 15:50:03 +0530

    refactor: Rebase code with Gitlab branch.(V1.0.2)

--- example/lib/src/screens/collage_sample.dart
+++ example/lib/src/screens/collage_sample.dart
@@ -26,7 +26,7 @@ class CollageSample extends StatefulWidget {
 }
 
 class _CollageSample extends State<CollageSample> {
-  GlobalKey _screenshotKey = GlobalKey();
+  final GlobalKey _screenshotKey = GlobalKey();
   bool _startLoading = false;
 
   @override
